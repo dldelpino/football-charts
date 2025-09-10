5cc22ec1756292d5a8de70c38573ed2e8477db9f
dldelpino <lopezdelpinodavid@gmail.com>
2025-09-10 15:32:36 +0200

    Deployment

--- quasar.config.js
+++ quasar.config.js
@@ -68,7 +68,7 @@ export default defineConfig((/* ctx */) => {
         }, { server: false }]
       ],
 
-      publicPath: './'
+      publicPath: '/football-charts/'
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#devserver
