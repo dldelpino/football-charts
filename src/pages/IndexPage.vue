5f7af19875868db647ecbdee9411a843d0e38095
dldelpino <lopezdelpinodavid@gmail.com>
2025-10-09 13:51:04 +0200

    Wake up backend on page load

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,7 +3,12 @@
 </template>
 
 <script setup>
-
+import { onMounted } from 'vue'
+onMounted(() => {
+  fetch("https://football-charts-backend.onrender.com/")
+    .then(() => {console.log("Backend loaded successfully")})
+    .catch(() => console.warn("Loading backend failed"))
+})
 </script>
 
 <style>
